cc5f18211b180b852ab948b130c4757780b4507b
jglasovic <jglasovic@gmail.com>
2018-01-06 15:11:06 +0100

    fix modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { withRouter } from "react-router-dom";
-import Routes from "./routes";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Routes from './routes';
 
 const App = props => (
   <div>
     <Routes {...props.location} />
   </div>
 );
+
 export default withRouter(App);
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import AppView from "./views/AppView";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import AppView from './views/AppView';
 
 const Routes = props => (
   <Switch>
