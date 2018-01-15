0318b6466c8377258cbc8fe355f363149f7f170d
jglasovic <jglasovic@gmail.com>
2018-01-15 15:00:16 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import Routes from './routes';
-
+// test
 const App = props => (
   <div>
     <Routes {...props.location} />
